cc608a8d8dfa77df16a6c733822a2f8977989f28
wangjianbing <wangjianbing@wangjianbingdeMacBook-Pro.local>
2016-05-06 16:05:58 +0800

    中间件组建

---
+++ principle/app.js
@@ -0,0 +1,67 @@
+/**
+ * Created by wangjianbing on 16/5/6.
+ */
+
+var express = require('express');
+
+
+
+//路由
+var router = express.Router();
+var subRouter = express.Router();
+
+//中间件
+
+function testMiddleWare(req, res, next) {
+
+
+    console.log("test middleWare");
+    //req.log = function(info) {
+    //
+    //    consoel.log(info);
+    //}
+    next();
+}
+
+function testMiddleWare2(req, res, next) {
+
+
+    console.log("test middleWare 222222");
+    //req.log = function(info) {
+    //
+    //    consoel.log(info);
+    //}
+    next();
+}
+
+router.use(testMiddleWare2);
+router.get('/', function(req, res) {
+
+    res.send("test router1");
+});
+
+router.get('/url1', function(req, res) {
+
+    res.send("test url11");
+});
+
+
+subRouter.get('/', function(req, res) {
+
+    res.send("test subRouter");
+});
+
+router.use('/subrouter', subRouter);
+var app = express();
+
+app.use(testMiddleWare);
+
+app.get('/', function(req, res) {
+
+    res.send("Hellw Express");
+});
+
+
+app.use('/router1', router);
+
+app.listen(3000);
